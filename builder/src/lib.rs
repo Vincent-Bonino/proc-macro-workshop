c042b02156c357696d70ae9491f7935775dbfcfe
Vincent Bonino <shadow23032000@gmail.com>
2025-01-11 21:31:49 +0100

    Add: Add builder part 4 & 5

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,51 +1,54 @@
 use proc_macro::TokenStream;
 
 use quote::quote;
+use syn::__private::Span;
 use syn::{parse_macro_input, DeriveInput, Ident, Visibility};
 use syn::{Data, DataStruct, Fields, FieldsNamed};
-use syn::__private::Span;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     // Parse the input tokens into a syntax tree
     let input_derive: DeriveInput = parse_macro_input!(input);
 
-    if let Data::Struct(
-        DataStruct{
-            fields: Fields::Named(
-                FieldsNamed {
-                    named,
-                    ..
-                }
-            ),
-            ..
-        }
-    ) = input_derive.data {
-        // Base structure
+    if let Data::Struct(DataStruct {
+        fields: Fields::Named(FieldsNamed { named, .. }),
+        ..
+    }) = input_derive.data
+    {
+        // ####################
+        // #  Base structure  #
+        // ####################
         let _vis: Visibility = input_derive.vis;
         let name: Ident = input_derive.ident;
 
-        let fields_it = named.iter().map(
-            |f| (f.ident.as_ref().expect("Unnamed field"), &f.ty)
-        );
+        let fields_it = named
+            .iter()
+            .map(|f| (f.ident.as_ref().expect("Unnamed field"), &f.ty));
+        let fields_ident = fields_it
+            .clone()
+            .map(|(i, _t)| i);
 
-        // Builder
+        // #############
+        // #  Builder  #
+        // #############
         let builder_name: Ident = Ident::new(&format!("{}Builder", name), Span::call_site());
 
-        let builder_fields = fields_it.clone().map(
-            |(i, t)| quote! { #i : ::core::option::Option<#t> }
-        );
-        let builder_setters = fields_it.clone().map(
-            |(i, t)| quote! {
+        let builder_fields = fields_it
+            .clone()
+            .map(|(i, t)| quote! { #i : ::core::option::Option<#t> });
+        let builder_setters = fields_it.clone().map(|(i, t)| {
+            quote! {
                 fn #i (&mut self, value: #t) -> &mut Self {
                     self.#i = ::core::option::Option::Some(value);
                     self
                 }
             }
-        );
+        });
 
         // Build the output, possibly using quasi-quotation
+
         let result = quote! {
+
             // Builder
             struct #builder_name {
                 #( #builder_fields ),*
@@ -53,6 +56,18 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
             impl #builder_name {
                 #( #builder_setters )*
+
+                pub fn build(&mut self) ->
+                    ::core::result::Result<Command, std::boxed::Box<dyn ::std::error::Error>> {
+
+                    Ok(
+                        #name {
+                            #(
+                                #fields_ident : self.#fields_ident.as_ref().ok_or("None field")?.clone()
+                            ),*
+                        }
+                    )
+                }
             }
 
             // Struct
